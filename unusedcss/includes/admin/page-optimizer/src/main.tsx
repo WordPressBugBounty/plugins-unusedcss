2fb57e6786f8b9d826e721d9bdd5bcfdf6a25712
Sean Murphy <sean@iamseanmurphy.com>
2024-12-06 10:18:07 +0000

    2.4.4

--- unusedcss/includes/admin/page-optimizer/src/main.tsx
+++ unusedcss/includes/admin/page-optimizer/src/main.tsx
@@ -9,13 +9,13 @@ import "@fontsource-variable/inter";
 import {createRoot} from 'react-dom/client';
 import ShadowRoot from "components/shadow-dom";
 import SpeedPopover from "app/speed-popover";
-import {isDev} from "lib/utils";
+import {isDev, disableDebugReport} from "lib/utils";
 import Providers from "./Providers";
 
 import Bugsnag from '@bugsnag/js'
 import BugsnagPluginReact, {BugsnagPluginReactResult} from '@bugsnag/plugin-react'
 
-if (!isDev) {
+if (!isDev && !disableDebugReport) {
     Bugsnag.start({
         appVersion: __OPTIMIZER_VERSION__,
         apiKey: '005f0d45718ad741e38cf9280457d034',
@@ -28,7 +28,7 @@ if (!isDev) {
     })
 }
 
-const plugin = !isDev ? Bugsnag.getPlugin("react") as BugsnagPluginReactResult : null;
+const plugin = !isDev && !disableDebugReport ? Bugsnag.getPlugin("react") as BugsnagPluginReactResult : null;
 export const ErrorBoundary = plugin ? plugin.createErrorBoundary(React) : null;
 
 interface initRapidLoadOptimizerProps {
